36edcf9c143dfe77fe829ed2bcd806dcbbc85ed7
Clostou <guoqing2234@gmail.com>
2025-06-01 23:48:25 +0800

    Update

---
+++ __init__.py
@@ -0,0 +1,22 @@
+"""
+ML Collection
++++++++++++++
+
+  自己实现的一些经典机器学习算法，包含许多可视化内容
+
+  关键模组：
+    - neural.py: 神经网络
+    - optimization.py: 优化算法
+    - reduce.py: 降维
+    - regress.py: 回归
+    - pre.py: 前处理
+    - post.py: 后处理
+    - BOA.py: 贝叶斯优化
+    - CNN.py: 卷积神经网络
+    - SVM.py: 支持向量机
+
+  作者：
+    仰望 (Github@clostou)
+"""
+
+
